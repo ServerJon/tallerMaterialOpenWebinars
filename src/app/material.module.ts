31c85b66e3fe6b153677be3448f1e27c17d36e01
ServerJon <ctsesiphon@hotmail.com>
2021-01-16 20:48:26 +0100

    Botón menú

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
